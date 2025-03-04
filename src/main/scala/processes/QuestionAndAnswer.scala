2f9fe14a11b8b8789ec872a67d3f8120b6a0a095
Dylan-Ranger <dylan.ranger.dev@gmail.com>
2025-03-04 19:53:40 +0200

    Refine Q&A program flow

--- src/main/scala/processes/QuestionAndAnswer.scala
+++ src/main/scala/processes/QuestionAndAnswer.scala
@@ -1,51 +1,24 @@
 package processes
 
 import traits.Process
+import scala.io.StdIn._
 /*
-* Encapsultes a single question and answer process.
+* Encapsulates a single question and answer process.
 */
-class QuestionAndAnswer[T](val question: String, val inputType: T) extends Process {
+class QuestionAndAnswer(val question: String) extends Process {
 
   def execute(): Unit = {
     var inputValid = false
-    var counter = 1
-    println(s"Count to ${inputType} with me. I'll start with 1")
 
     while (inputValid == false) {
-      println("Enter the next number")
-      var userInput = readLine()
-
-      // Basic input validation structure
-      var moveToNext = false
-      while (moveToNext == false) {
-        val isValidInput = validateInput(userInput, counter + 1)
-
-        if (isValidInput == true) {
-          moveToNext = true
-        } else {
-          println("That is not the correct input. Try again")
-          userInput = readLine()
-        }
-      }
-
-      println("Well Done!")
-      counter += 1
-
-      if (counter == inputType) {
-        inputValid = true
-      }
+      println(s"$question")
+      val userInput = readLine()
+      inputValid = validateInput(userInput)
     }
   }
 
-  private def validateInput(input: String, counter: Int): Boolean = {
-    try {
-      if (Integer.parseInt(input) == counter) {
-        true
-      } else {
-        false
-      }
-    } catch {
-      case error: Error => false
-    }
+  private def validateInput(input: String): Boolean = {
+    val validator = new InputValidator(question, input)
+    validator.execute()
   }
 }
